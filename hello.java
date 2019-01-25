03baacc63d503f366183d16a05585bbd8a9b0ee0
sugapriyad0206 <35481863+sugapriyad0206@users.noreply.github.com>
2019-01-25 18:09:18 +0530

    Add files via upload

---
+++ hello.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.util.Scanner;
+class hello
+{
+	public static void main(String args[])
+	{
+		int N;
+		Scanner s=new Scanner(System.in);
+		N=s.nextInt();
+		for(int i=0;i<N;i++)
+			System.out.println("Hello");
+	}
+}
